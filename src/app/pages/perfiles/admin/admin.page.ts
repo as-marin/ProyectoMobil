c29188be84f6d865bc32ca5e421764c8ed81084a
Fabian Mella Barra <fa.mella@duocuc.cl>
2024-09-24 16:44:17 -0300

    Edit button exit

--- src/app/pages/perfiles/admin/admin.page.ts
+++ src/app/pages/perfiles/admin/admin.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminPage implements OnInit {
 
-  constructor() { }
+  constructor(private navCtrl: NavController) { }
 
   ngOnInit() {
   }
 
+  goBack() {
+    this.navCtrl.back();
+  }
 }
