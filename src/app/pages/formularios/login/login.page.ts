2ac7d2de99d5228946f0f936c749ccf34e9f849b
Fabian Mella Barra <fa.mella@duocuc.cl>
2024-09-25 22:10:18 -0300

    Update login ts
    add validation

--- src/app/pages/formularios/login/login.page.ts
+++ src/app/pages/formularios/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { NgForm, NgModel } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,12 +13,26 @@ export class LoginPage implements OnInit {
     email: '',
     password: ''
   };
-  constructor(private navCtrl: NavController) {}  
+  constructor(private navCtrl: NavController, private router: Router) {}  
 
   ngOnInit() {}
 
-  onSubmit() {
-    console.log('hello');
+  validateEmail(emailField: NgModel) {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    if (!emailPattern.test(this.loginData.email)) {
+      emailField.control.setErrors({ invalidEmail: true });
+    } else {
+      emailField.control.setErrors(null);
+    }
+  }
+
+  onSubmit(formularioLogin: NgForm) {
+    if (formularioLogin.valid) {
+      console.log('Formulario válido:', this.loginData);
+      this.router.navigate(['/user']);
+    } else {
+      console.log('Formulario no válido');
+    }
   }
 
 
