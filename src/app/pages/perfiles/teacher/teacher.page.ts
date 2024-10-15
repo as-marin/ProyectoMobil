12350e85e0e056feb27bfbb866a23196fab6c5ab
CETECOM <fa.mella@duocuc.cl>
2024-10-15 16:33:58 -0300

    add perfil teacher

--- src/app/pages/perfiles/teacher/teacher.page.ts
+++ src/app/pages/perfiles/teacher/teacher.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-teacher',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeacherPage implements OnInit {
 
-  constructor() { }
+  constructor(private navCtrl: NavController) { }
 
   ngOnInit() {
   }
 
+  goBack() {
+    this.navCtrl.back();
+  }
 }
