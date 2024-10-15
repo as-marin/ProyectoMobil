c2632bc7098efc47fe678c76a88d46eb55116748
CETECOM <fa.mella@duocuc.cl>
2024-10-15 16:53:08 -0300

    update teacher html and ts

--- src/app/pages/perfiles/teacher/teacher.page.ts
+++ src/app/pages/perfiles/teacher/teacher.page.ts
@@ -8,6 +8,11 @@ import { NavController } from '@ionic/angular';
 })
 export class TeacherPage implements OnInit {
 
+  classes = [
+    { name: 'Ingeniería de Software', hour: 'Martes 17:30 - 18:30 hrs.', avatar: 'assets/img/john-doe.png' },
+    { name: 'Programación de algoritmos', hour: 'Martes 19:00 - 19:45 hrs.', avatar: 'assets/img/jane-smith.png' },
+  ];
+
   constructor(private navCtrl: NavController) { }
 
   ngOnInit() {
@@ -16,4 +21,9 @@ export class TeacherPage implements OnInit {
   goBack() {
     this.navCtrl.back();
   }
+
+  viewUserDetails(user: any) {
+    console.log('User details:', user);
+  }
+
 }
