f2c8ae27fd2cdfd27d8efbd4adac9e9590fb96cd
CETECOM <fa.mella@duocuc.cl>
2024-10-24 17:27:03 -0300

    cositas

--- src/app/components/scanner/scanner.component.ts
+++ src/app/components/scanner/scanner.component.ts
@@ -1,14 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { Barcode, BarcodeScanner } from '@capacitor-mlkit/barcode-scanning';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-scanner',
   templateUrl: './scanner.component.html',
   styleUrls: ['./scanner.component.scss'],
 })
-export class ScannerComponent  implements OnInit {
+export class ScannerComponent  implements OnInit{
+  AlertController: any;
 
   constructor() { }
 
-  ngOnInit() {}
+  isSupported = false;
+  barcodes: Barcode[] = [];
+
+  ngOnInit() {
+    BarcodeScanner.isSupported().then((result) => {
+      this.isSupported = result.supported;
+    });
+  }
+
+  async scan(): Promise<void> {
+    const granted = await this.requestPermissions();
+    if (!granted) {
+      this.presentAlert();
+      return;
+    }
+    const { barcodes } = await BarcodeScanner.scan();
+    this.barcodes.push(...barcodes);
+  }
+
+  async requestPermissions(): Promise<boolean> {
+    const { camera } = await BarcodeScanner.requestPermissions();
+    return camera === 'granted' || camera === 'limited';
+  }
+
+  async presentAlert(): Promise<void> {
+    const alert = await this.AlertController.create({
+      header: 'Permiso denegado',
+      message: 'Para usar la aplicación necesita autorizar los permisos de la cámara.',
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
 
 }
