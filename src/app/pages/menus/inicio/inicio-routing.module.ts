0d0de25912f45dec09440c1a2e379fe69e60751c
Fabian Mella Barra <fa.mella@duocuc.cl>
2024-11-30 17:17:45 -0300

    Update inicio-routing.module.ts
    Eliminacion de rutas innecesarias en la pagina de inicio que generaban conflicto con los routterlink

--- src/app/pages/menus/inicio/inicio-routing.module.ts
+++ src/app/pages/menus/inicio/inicio-routing.module.ts
@@ -8,25 +8,10 @@ const routes: Routes = [
     path: '',
     component: InicioPage
   },
-  {
-    
-      path: 'user',
-      loadChildren: () => import('../../perfiles/user/user.module').then( m => m.UserPageModule)
-    
-  },
-  {
-
-    path: 'scan',
-    loadChildren: () => import('../scan/scan.module').then( m => m.ScanPageModule)
-  },
-  {
-    path: 'inscripcion',
-    loadChildren: () => import('../inscripcion/inscripcion.module').then( m => m.InscripcionPageModule)
-  }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class InicioPageRoutingModule {}
+export class InicioPageRoutingModule { }
