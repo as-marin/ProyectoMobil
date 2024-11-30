75a2f38e7fc93859b860ece40f3cf76c22984847
Fabian Mella Barra <fa.mella@duocuc.cl>
2024-11-30 17:33:12 -0300

    Update sections.service.ts
    Refinacion de las funciones para el mejor manejo de la const data

--- src/app/services/sections.service.ts
+++ src/app/services/sections.service.ts
@@ -2,33 +2,58 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable, map } from 'rxjs';
 
-
 @Injectable({
-    providedIn: 'root'
+    providedIn: 'root',
 })
 export class SectionsService {
-
     constructor(private firestore: AngularFirestore) { }
 
     // Obtener las secciones disponibles en Firestore
     getSections(uid: string): Observable<any[]> {
-        return this.firestore.collection(`users/${uid}/sections`).snapshotChanges().pipe(
-            map(sections => sections.map(section => {
-                const data = section.payload.doc.data() as { nombre?: string };
-                //console.log('Datos de la sección:', data); // Depuración
-                return { ...data };
-            }))
-        );
+        return this.firestore
+            .collection(`users/${uid}/sections`)
+            .snapshotChanges()
+            .pipe(
+                map((sections) =>
+                    sections.map((section) => {
+                        const data = section.payload.doc.data() as { nombre?: string };
+                        //console.log('Datos de la sección:', data); // Depuración
+                        return { ...data };
+                    })
+                )
+            );
+    }
+
+    getStudentAttendance(studentEmail: string): Observable<any[]> {
+        return this.firestore
+            .collectionGroup('attendance', (ref) =>
+                ref.where('user.email', '==', studentEmail)
+            )
+            .snapshotChanges()
+            .pipe(
+                map((actions) =>
+                    actions.map((a) => {
+                        const data = a.payload.doc.data() as Record<string, any>; // Asegúrate de que data sea un objeto
+                        const id = a.payload.doc.id;
+                        return { id, ...data };
+                    })
+                )
+            );
     }
 
     getAllSections(): Observable<any[]> {
-        return this.firestore.collection('sections').snapshotChanges().pipe(
-            map(sections => sections.map(section => {
-                const data = section.payload.doc.data() as { nombre?: string };
-                //console.log('Datos de la sección:', data); // Depuración
-                return { ...data };
-            }))
-        );
+        return this.firestore
+            .collection('sections')
+            .snapshotChanges()
+            .pipe(
+                map((sections) =>
+                    sections.map((section) => {
+                        const data = section.payload.doc.data() as { nombre?: string };
+                        //console.log('Datos de la sección:', data); // Depuración
+                        return { ...data };
+                    })
+                )
+            );
     }
 
     // Inscribir a un usuario en una sección
@@ -36,5 +61,4 @@ export class SectionsService {
         const userRef = this.firestore.collection(`users/${uid}/sections`);
         await userRef.doc(sectionId).set({ enrolled: true });
     }
-
 }
