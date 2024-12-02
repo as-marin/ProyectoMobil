46f7747764b6fc0250b5c984bb142b1d9a24dda0
Fabian Mella Barra <fa.mella@duocuc.cl>
2024-12-02 16:45:18 -0300

    Update environment.ts
    Configuracion del entorno de firebase en el entorno de typscript

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyA9PU8bdgRg3fCVKT4M7I5jpq8lNdrmzts",
+    authDomain: "proyectomobil-45915.firebaseapp.com",
+    projectId: "proyectomobil-45915",
+    storageBucket: "proyectomobil-45915.appspot.com",
+    messagingSenderId: "104935696813",
+    appId: "1:104935696813:web:f0f6bc2e54c1dc61ae1924"
+  }
 };
 
 /*
